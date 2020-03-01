e847e4bbb27d4f581545480fbe6fc92241519fc6
Guillermo Vizcaino <guillermovizcaino1999@gmail.com>
2020-03-01 15:15:05 +0100

    Terminado el ej2 al 100%

--- fmpokedex.py
+++ fmpokedex.py
@@ -47,6 +47,46 @@ def lista_pokemon_y_sus_evoluciones(datos):
 
 ## **Contar Informacion:** ¿De que quieres contar informacion? (Dentro del programa habra un submenu en el que podras elegir que informacion quieres contar.)
 
+## Contador de numero de pokemons que existen
+
+def num_pokemon(datos):
+
+    pokemons=[]
+
+    for pokemon in datos["pokemon"]:
+
+    	pokemons.append(pokemon["name"])
+
+    return len(pokemons)
+
+
+## Contador de numeros de pokemons que nacen de un huevo.
+
+    ## pnd -> pueden nacer de
+def num_pokemon_pnd_huevo(datos):
+
+    pokemons=[]
+
+    for pokemon in datos["pokemon"]:
+        huevo=pokemon["egg"]
+
+        if huevo != "Not in Eggs":
+            pokemons.append(pokemon["name"])
+
+    return len(pokemons)
+
+## Contador de numero de tipos distintos
+
+def num_tipos_dpokemon(datos):
+
+    tipos=[]
+
+    for pokemon in datos["pokemon"]:
+        for tiposdpokemon in pokemon["type"]:
+            if tiposdpokemon not in tipos:
+                tipos.append(tiposdpokemon)
+
+    return len(tipos)
 
 
 ## **Buscar o filtrar Informacion:** Pide el nombre de un pokemon y muestra su tipo.
--- menu_pokedex.py
+++ menu_pokedex.py
@@ -6,51 +6,87 @@ with open("pokedex.json") as fichero:
 	datos=json.load(fichero)
 
 while True:
-    print()
-    print()
-    print("\t------ MENU DE OPCIONES ------")
-    print()
-    print("\t1. Listar informacion.")
-    print("\t2. Contar informacion.")
-    print("\t3. Buscar o filtrar informacion.")
-    print("\t4. Buscar informacion relacionada.")
-    print("\t5. Ejercicio libre.")
-    print("\t0. Salir del programa.")
-    print()
-    print("\t------------------------------")
-    print()
-    opcion=int(input("\tOpcion: "))
+	print()
+	print()
+	print("\t------ MENU DE OPCIONES ------")
+	print()
+	print("\t1. Listar informacion.")
+	print("\t2. Contar informacion.")
+	print("\t3. Buscar o filtrar informacion.")
+	print("\t4. Buscar informacion relacionada.")
+	print("\t5. Ejercicio libre.")
+	print("\t0. Salir del programa.")
+	print()
+	print("\t------------------------------")
+	print()
+	opcion=int(input("\tOpcion: "))
 
-    if opcion ==0:
-        break;
+	if opcion ==0:
+		break
 
-    elif opcion ==1:
-        print()
-        print("\t--- Lista con los nombres de los pokemons y su evolucion ---")
-        for pye in lista_pokemon_y_sus_evoluciones(datos):
-            print(pye)
-        print()
+	elif opcion ==1:
+		print()
+		print("\t--- Lista con los nombres de los pokemons y su evolucion ---")
+		for pye in lista_pokemon_y_sus_evoluciones(datos):
+			print(pye)
 
-    elif opcion == 2:
-        print()
+		print()
 
-        print()
+	elif opcion == 2:
+		while True:
+			print()
+			print()
+			print("\t------ MENU DE CONTAR ------")
+			print()
+			print("\t1. Número de pokemons que existen.")
+			print("\t2. Número de pokemons que nacen de un huevo.")
+			print("\t3. Número de tipos distintos de pokemons.")
+			print("\t0. Salir del programa.")
+			print()
+			print("\t------------------------------")
+			print()
+			opcion=int(input("\tOpcion: "))
 
-    elif opcion == 3:
-        print()
+			if opcion==0:
+				break
 
-        print()
+			elif opcion==1:
+				print()
+				print("\t--- Número de pokemons que existen ---")
+				print()
+				print("\t   Hay un total de %i pokemons registrados en la pokedex."%(num_pokemon(datos)))
+				print()
 
-    elif opcion == 4:
-        print()
+			elif opcion==2:
+				print()
+				print("\t--- Número de pokemons que nacen de un huevo ---")
+				print()
+				print("\t   Hay %i pokemons que pueden nacer de un huevo."%(num_pokemon_pnd_huevo(datos)))
+				print()
 
-        print()
+			elif opcion==3:
+				print()
+				print("\t--- Número de tipos distintos de pokemons ---")
+				print()
+				print("\t   Hay %i tipos distintos de pokemons."%(num_tipos_dpokemon(datos)))
+				print()
 
-    elif opcion == 5:
-        print()
+			else:
+				print("\tError, opción incorrecta.")
 
-        print()
-    else:
-        print()
-        print("\tError. opcion incorrecta")
-        print()
+	elif opcion == 3:
+		print()
+		print()
+
+	elif opcion == 4:
+		print()
+		print()
+
+	elif opcion == 5:
+		print()
+		print()
+
+	else:
+		print()
+		print("\tError. opcion incorrecta")
+		print()
